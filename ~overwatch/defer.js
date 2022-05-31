4106597bda6b1537044f8478d3fedadedbc6f0d3
fatolive <parkele1@navr.com>
2022-05-31 18:25:15 +0900

    Start project

--- ~overwatch/defer.js
+++
@@ -1,9 +0,0 @@
-// const boxEls = document.querySelectorAll('.box');
-// boxEls.forEach(function(boxEl, index){
-  //   boxEl.classList.add(`order-${index+1}`);
-  //   console.log(index, boxEl);
-  // });
-const boxEl = document.querySelector('.box');
-console.log(boxEl.textContent);
-boxEl.textContent = "HEROPY";
-console.log(boxEl.textContent);
